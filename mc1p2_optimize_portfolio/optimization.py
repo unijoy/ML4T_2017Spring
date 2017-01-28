e8e6c61eaba41336ff36197f87ac07c2f77a1c63
lee lim <leelim81@gmail.com>
2017-01-29 01:29:30 +0800

    : sdasdas :qwq wq

--- mc1p2_optimize_portfolio/optimization.py
+++ mc1p2_optimize_portfolio/optimization.py
@@ -11,6 +11,69 @@ from util import get_data, plot_data
 def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
     syms=['GOOG','AAPL','GLD','XOM'], gen_plot=False):
 
+    ######### ASSESS PORTFOLIO
+    def assess_portfolio(sd=dt.datetime(2008, 1, 1), ed=dt.datetime(2009, 1, 1), \
+                     syms=['GOOG', 'AAPL', 'GLD', 'XOM'], \
+                     allocs=[0.1, 0.2, 0.3, 0.4], rfr=0.0, sf=252.0):
+        import math
+        # Read in adjusted closing prices for given symbols, date range
+        dates = pd.date_range(sd, ed)
+        prices_all = get_data(syms, dates)  # automatically adds SPY
+        prices = prices_all[syms]  # only portfolio symbols
+        prices_SPY = prices_all['SPY']  # only SPY, for comparison later
+
+        # Get daily portfolio value
+        port_val = prices_SPY  # add code here to compute daily portfolio values
+        df = prices.copy()
+        df[1:] = (df[1:] / df[0:-1].values)
+        for j in range(len(allocs)):
+            df.ix[0, j] = allocs[j]
+
+        allocs = np.asarray([0.2, 0.2, 0.3, 0.3, 0.0])
+
+        days = prices.shape[0]
+        for i in range(1, days):
+            df.ix[i, :] = df.ix[i, :] * df.ix[i - 1, :]
+        df['cumm'] = df.sum(axis=1)
+        df['daily'] = (df.ix[1:, 'cumm'] / df.ix[0:-1, 'cumm'].values) - 1.
+        df['daily_rf'] = df['daily'] - rfr ** (1. / days)
+        sr = math.sqrt(sf) * df['daily_rf'].mean() / df['daily_rf'].std()
+        cr = df.ix[-1, 'cumm'] - 1
+        sddr = df.ix[1:, 'daily'].std()
+        adr = df.ix[1:, 'daily'].mean()
+        return cr, adr, sddr, sr
+
+    def get_negative_sharpe(allocs,prices ):
+        import math
+        rfr=0.0
+        sf=252.0
+        df = prices.copy()
+        df[1:] = (df[1:] / df[0:-1].values)
+        for j in range(len(allocs)):
+            df.ix[0, j] = allocs[j]
+
+        days = prices.shape[0]
+        for i in range(1, days):
+            df.ix[i, :] = df.ix[i, :] * df.ix[i - 1, :]
+        df['cumm'] = df.sum(axis=1)
+        df['daily'] = (df.ix[1:, 'cumm'] / df.ix[0:-1, 'cumm'].values) - 1.
+        df['daily_rf'] = df['daily'] - rfr ** (1. / days)
+        return math.sqrt(sf) * df['daily_rf'].mean() / df['daily_rf'].std() *-1.0
+
+    import scipy.optimize as spo
+
+    def min_sharpe(data,error_func):
+        allocs = np.asarray([0.2, 0.2, 0.3, 0.3, 0.0])
+
+        #Says one minus the sum of all variables must be zero
+        cons = ({'type': 'eq', 'fun': lambda x:  1 - sum(x)})
+
+        #Required to have non negative values
+        bnds = tuple((0.0,1.0) for x in allocs)
+
+        result = spo.minimize(error_func,allocs,args=(data,),method='SLSQP',options={'disp':True},constraints=cons,bounds=bnds)
+        return result.x
+
     # Read in adjusted closing prices for given symbols, date range
     dates = pd.date_range(sd, ed)
     prices_all = get_data(syms, dates)  # automatically adds SPY
@@ -18,18 +81,25 @@ def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
     prices_SPY = prices_all['SPY']  # only SPY, for comparison later
 
     # find the allocations for the optimal portfolio
-    # note that the values here ARE NOT meant to be correct for a test case
     allocs = np.asarray([0.2, 0.2, 0.3, 0.3, 0.0]) # add code here to find the allocations
     cr, adr, sddr, sr = [0.25, 0.001, 0.0005, 2.1] # add code here to compute stats
 
+    allocs = min_sharpe(prices,get_negative_sharpe)
+    cr, adr, sddr, sr = assess_portfolio(sd = sd, ed = ed,syms = syms, allocs = allocs)
+
     # Get daily portfolio value
     port_val = prices_SPY # add code here to compute daily portfolio values
+    port_val = prices_all['GOOG']
 
     # Compare daily portfolio value with SPY using a normalized plot
     if gen_plot:
-        # add code to plot here
         df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
-        pass
+        ax = df_temp.plot(title="Stock prices", fontsize=12)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Price")
+        plt.show()
+        fig = ax.get_figure()
+        fig.savefig("comparison_optimal.png")
 
     return allocs, cr, adr, sddr, sr
 
